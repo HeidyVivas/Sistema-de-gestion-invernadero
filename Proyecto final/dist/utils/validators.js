465b3cbddf89764196f124fc5b9653bdf70eccc8
HeidyVivas <hvivas2208@gmail.com>
2025-10-02 23:57:44 -0500

    Modificacion de carpetas

--- Proyecto final/dist/utils/validators.js
+++
@@ -1,7 +0,0 @@
-import { ValidationError } from "../domain/errors/ValidationError.js";
-export function validarTexto(campo, valor) {
-    if (!valor || valor.trim() === "") {
-        throw new ValidationError(`El campo ${campo} no puede estar vacío`);
-    }
-}
-//# sourceMappingURL=validators.js.map
